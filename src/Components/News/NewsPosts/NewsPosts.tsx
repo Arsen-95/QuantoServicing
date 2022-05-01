577a164079b39839fa69ff6b2a6429f0127ae90e
Arsen <suleymenovarsen@mail.ru>
2022-05-01 18:55:09 +0500

    last try

--- src/Components/News/NewsPosts/NewsPosts.tsx
+++ src/Components/News/NewsPosts/NewsPosts.tsx
@@ -1,10 +1,10 @@
 import { Box, Container, Flex } from "@chakra-ui/react";
 import { useState } from "react";
+import { useTranslation } from "react-i18next";
 
 import { Tabs } from "Components/Common/Buttons";
 import { PostItem } from "Components/Common/PostItem";
 import Pagination from "./Pagination";
-import { useTranslation } from "react-i18next";
 
 export const NewsPosts = () => {
   const { t } = useTranslation("headers");
@@ -20,12 +20,17 @@ export const NewsPosts = () => {
     },
   ];
 
-  const [event, setEvent] = useState("news");
+  const [eventType, setEventType] = useState("news");
+  // const [posts, setPosts] = useState(news);
 
   return (
     <Box background="#23242B" pt="100px" pb="40px">
       <Container>
-        <Tabs data={tabsList} event={event} setEvent={setEvent} />
+        <Tabs
+          data={tabsList}
+          setEventType={setEventType}
+          eventType={eventType}
+        />
         <Flex
           maxW={["280px", "280px", "628px", "710px", "1083px"]}
           margin="0 auto"
@@ -33,50 +38,7 @@ export const NewsPosts = () => {
           gap={["30px", "26px", "28px", "46px"]}
           mb="100px"
         >
-          <PostItem
-            picture="post1.png"
-            text="Социальное обеспечение работника, включая социальное страхование, – это одно из основных обязательств, которое берет на себя работодатель. "
-          />
-          <PostItem
-            picture="post1.png"
-            text="Социальное обеспечение работника, включая социальное страхование, – это одно из основных обязательств, которое берет на себя работодатель. "
-          />
-          <PostItem
-            picture="post1.png"
-            text="Социальное обеспечение работника, включая социальное страхование, – это одно из основных обязательств, которое берет на себя работодатель. "
-          />
-          <PostItem
-            picture="post1.png"
-            text="Социальное обеспечение работника, включая социальное страхование, – это одно из основных обязательств, которое берет на себя работодатель. "
-          />
-          <PostItem
-            picture="post1.png"
-            text="Социальное обеспечение работника, включая социальное страхование, – это одно из основных обязательств, которое берет на себя работодатель. "
-          />
-          <PostItem
-            picture="post1.png"
-            text="Социальное обеспечение работника, включая социальное страхование, – это одно из основных обязательств, которое берет на себя работодатель. "
-          />
-          <PostItem
-            picture="post1.png"
-            text="Социальное обеспечение работника, включая социальное страхование, – это одно из основных обязательств, которое берет на себя работодатель. "
-          />
-          <PostItem
-            picture="post1.png"
-            text="Социальное обеспечение работника, включая социальное страхование, – это одно из основных обязательств, которое берет на себя работодатель. "
-          />
-          <PostItem
-            picture="post1.png"
-            text="Социальное обеспечение работника, включая социальное страхование, – это одно из основных обязательств, которое берет на себя работодатель. "
-          />
-          <PostItem
-            picture="post1.png"
-            text="Социальное обеспечение работника, включая социальное страхование, – это одно из основных обязательств, которое берет на себя работодатель. "
-          />
-          <PostItem
-            picture="post1.png"
-            text="Социальное обеспечение работника, включая социальное страхование, – это одно из основных обязательств, которое берет на себя работодатель. "
-          />
+          {/* <PostItem /> */}
         </Flex>
         <Pagination />
       </Container>
