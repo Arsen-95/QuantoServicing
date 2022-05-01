59631b81207e08c93b3339154793ccc70cca74f3
Arsen <suleymenovarsen@mail.ru>
2022-05-01 23:02:09 +0500

    ya uje ustal

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   i18n: {
     defaultLocale: 'ru',
     locales: ['ru', 'uz-cyrl', 'uz-latn', 'en'],
-    localePath: path.resolve('/.public/locales')
+    localePath: path.resolve('./public/locales')
   },
   
 }
